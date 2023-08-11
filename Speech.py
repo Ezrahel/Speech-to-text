d9a7fdc342967139acafaf5bbd97afe2f497d6b3
Ezrahel <86208443+Ezrahel@users.noreply.github.com>
2023-08-11 11:32:46 +0100

    Update Speech.py
    adjusted

--- Speech.py
+++ Speech.py
@@ -1,21 +1,21 @@
-import speech_recognition as sr
+# import speech_recognition as sr
 
-# Create a recognizer object
-r = sr.Recognizer()
+# # Create a recognizer object
+# r = sr.Recognizer()
 
-# Use the microphone as the audio source
-with sr.Microphone() as source:
-    print("Speak something...")
-    audio = r.listen(source)
+# # Use the microphone as the audio source
+# with sr.Microphone() as source:
+#     print("Speak something...")
+#     audio = r.listen(source)
 
-try:
-    # Perform speech recognition
-    text = r.recognize_google(audio)
-    print("You said:", text)
-except sr.UnknownValueError:
-    print("Sorry, I could not understand audio.")
-except sr.RequestError as e:
-    print("Sorry, an error occurred. {0}".format(e))
+# try:
+#     # Perform speech recognition
+#     text = r.recognize_google(audio)
+#     print("You said:", text)
+# except sr.UnknownValueError:
+#     print("Sorry, I could not understand audio.")
+# except sr.RequestError as e:
+#     print("Sorry, an error occurred. {0}".format(e))
 
 import speech_recognition as sr
 
